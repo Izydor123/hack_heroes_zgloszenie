3fb56b0555492aca7d843f2c099330986cfa56ad
Izydor123 <164671009+Izydor123@users.noreply.github.com>
2024-11-13 19:20:44 +0100

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -5,8 +5,8 @@ from datetime import timedelta
 celery.conf.beat_schedule = {
     'update-data-every-hour': {
         'task': 'tasks.update_data_task',
-        'schedule': timedelta(hours=24),
-    },
+        'schedule': timedelta(hours=24)
+    }
 }
 
 if __name__ == "__main__":
