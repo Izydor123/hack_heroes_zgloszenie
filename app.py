66265fa02e0ee843193bcb793074db5c6a1322b7
Izydor123 <164671009+Izydor123@users.noreply.github.com>
2024-11-15 12:34:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from eurodata import import_data, data_chart
 from pldata import PLWasteCharts
 from celery import Celery
